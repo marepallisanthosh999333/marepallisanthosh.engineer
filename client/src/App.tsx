215f325856d27e836546a1a7e1313218b54a46b3
Marepalli Santhosh <marepallisanthosh.999333@gmail.com>
2025-07-26 15:03:54 +0530

    Update favicon and site title
    - Set favicon to horse.png icon
    - Update page title to 'Marepalli Santhosh Portfolio'
    - Add dynamic title setting in App component
    - Replace default browser icon with custom horse icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -10,6 +10,9 @@ import Footer from './components/Footer';
 import AnimatedSection from './components/AnimatedSection';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Marepalli Santhosh Portfolio';
+  }, []);
   return (
     <div className="min-h-screen bg-gray-50 text-gray-900 overflow-x-hidden">
       <Header />
